1b9eb3b3c36a4e87bd69c72ce4045466c63f3528
Alex Werner <alexwerner@wi.rr.com>
2016-08-31 16:32:08 -0500

    Fixed Runtime Errors
    Added Forge to build path
    Corrected proxy file location

--- src/main/java/com/CubeStorm/nuclearWorkshop/Reference.java
+++ src/main/java/com/CubeStorm/nuclearWorkshop/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 	public static final String NAME = "Nuclear Workshop";
 	public static final String VERSION = "0.0.1a";
 	
-	public static final String CLIENT_PROXY_CLASS = "com.CubeStorm.nuclearWorkshop.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "com.CubeStorm.nuclearWorkshop.ServerProxy";
+	public static final String CLIENT_PROXY_CLASS = "com.CubeStorm.nuclearWorkshop.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.CubeStorm.nuclearWorkshop.proxy.ServerProxy";
 }
